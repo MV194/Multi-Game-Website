d3b8e86d92578425f6eb193015e32410b961c001
MykhayloVoroshylov <161176411+MykhayloVoroshylov@users.noreply.github.com>
2024-10-28 14:24:44 -0400

    Update JS_Tic-Tac-Toe.js

--- JS_Tic-Tac-Toe.js
+++ JS_Tic-Tac-Toe.js
@@ -1,1 +1,22 @@
 //In Development
+
+
+
+//Modal function
+var rules_modal = document.getElementById("Rules-Modal");
+var rules_button = document.getElementById("Rules");
+var closebutton = document.getElementsByClassName("close-button")[0];
+
+rules_button.onclick = function() {
+    rules_modal.style.display = "block"; // Corrected: added quotes around "block"
+}
+
+closebutton.onclick = function() {
+    rules_modal.style.display = "none"; // Corrected: added quotes around "none"
+}
+
+window.onclick = function(event) {
+    if (event.target == rules_modal) {
+        rules_modal.style.display = "none";
+    }
+}
